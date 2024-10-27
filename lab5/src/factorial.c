e022d89a6487bc3a8becab62759f7ab9f12a3a6c
EmelianovG <47709477+EmelianovG@users.noreply.github.com>
2024-10-27 08:56:20 +0000

    5.1

---
+++ lab5/src/factorial.c
@@ -0,0 +1,53 @@
+int main(int argc, char** argv) {
+    int k = -1;
+    int pnum = -1;
+    int mod = -1;
+
+    while (1) {
+        int current_optind = optind ? optind : 1;
+
+        static struct option options[] = {
+            {"k", required_argument, 0, 0},
+            {"pnum", required_argument, 0, 0},
+            {"mod", required_argument, 0, 0},
+            {0, 0, 0, 0}
+        };
+
+        int option_index = 0;
+        int c = getopt_long(argc, argv, "f", options, &option_index);
+
+        if (c == -1) break;
+
+        switch (c) {
+            case 0:
+                switch (option_index) {
+                    case 0:
+                        k = atoi(optarg);
+                        break;
+                    case 1:
+                        pnum = atoi(optarg);
+                        break;
+                    case 2:
+                        mod = atoi(optarg);
+                        break;
+                    defalut:
+                        printf("Index %d is out of options\n", option_index);
+                }
+                break;
+            case '?':
+                break;
+            default:
+                printf("getopt returned character code 0%o?\n", c);
+        }
+    }
+
+    if (optind < argc) {
+        printf("Has at least one no option argument\n");
+        return 1;
+    }
+
+    if (k == -1 || pnum == -1 || mod == -1) {
+        printf("Usage: --k num --pnum num --mod num\n");
+        return 1;
+    }
+}
